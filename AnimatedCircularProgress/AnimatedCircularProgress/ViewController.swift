0d06271402955606faf956decd7206f8978e7bf7
Lahari Ganti <gplahari@gmail.com>
2019-03-31 22:51:27 -0700

    .

--- AnimatedCircularProgress/AnimatedCircularProgress/ViewController.swift
+++ AnimatedCircularProgress/AnimatedCircularProgress/ViewController.swift
@@ -8,15 +8,21 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, URLSessionDownloadDelegate {
     let shapeLayer = CAShapeLayer()
     let trackLayer = CAShapeLayer()
+    let percentageLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Start"
+        label.textAlignment = .center
+        label.font = UIFont.boldSystemFont(ofSize: 25)
+        return label
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
-        let center = view.center
-        let circularPath = UIBezierPath(arcCenter: center, radius: 100, startAngle: -CGFloat.pi / 2, endAngle: 2 * CGFloat.pi, clockwise: true)
+        let circularPath = UIBezierPath(arcCenter: .zero, radius: 80, startAngle: 0, endAngle: 2 * CGFloat.pi, clockwise: true)
 
         trackLayer.path = circularPath.cgPath
 
@@ -24,20 +30,51 @@ class ViewController: UIViewController {
         trackLayer.lineWidth = 10
         trackLayer.lineCap = .round
         trackLayer.fillColor = UIColor.clear.cgColor
+        trackLayer.position = view.center
         view.layer.addSublayer(trackLayer)
 
         shapeLayer.path = circularPath.cgPath
+        shapeLayer.position = view.center
         shapeLayer.strokeColor = UIColor.red.cgColor
         shapeLayer.lineWidth = 10
-        shapeLayer.lineCap = .round
         shapeLayer.strokeEnd = 0
+        shapeLayer.lineCap = .round
         shapeLayer.fillColor = UIColor.clear.cgColor
+        shapeLayer.transform = CATransform3DMakeRotation(-CGFloat.pi / 2, 0, 0, 1)
         view.layer.addSublayer(shapeLayer)
 
+        view.addSubview(percentageLabel)
+        percentageLabel.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+        percentageLabel.center = view.center
         view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(objectTapped)))
     }
 
-    @objc func objectTapped() {
+    let urlString = "https://firebasestorage.googleapis.com/v0/b/firestorechat-e64ac.appspot.com/o/intermediate_training_rec.mp4?alt=media&token=e20261d0-7219-49d2-b32d-367e1606500c"
+
+    private func beginDownloading() {
+        shapeLayer.strokeEnd = 0
+        print("downloading")
+        let configuration = URLSessionConfiguration.default
+        let operationQueue = OperationQueue()
+        let urlSession = URLSession(configuration: configuration, delegate: self, delegateQueue: operationQueue)
+        guard let string = URL(string: urlString) else { return }
+        let downloadTask = urlSession.downloadTask(with: string)
+        downloadTask.resume()
+    }
+
+    func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didFinishDownloadingTo location: URL) {
+        print("done")
+    }
+
+    func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didWriteData bytesWritten: Int64, totalBytesWritten: Int64, totalBytesExpectedToWrite: Int64) {
+        let percentage = CGFloat(totalBytesWritten)  / CGFloat(totalBytesExpectedToWrite)
+        DispatchQueue.main.async {
+            self.shapeLayer.strokeEnd = percentage
+            self.percentageLabel.text = "\(Int(percentage * 100)) %"
+        }
+    }
+
+    fileprivate func animateCircle() {
         let basicANimation = CABasicAnimation(keyPath: "strokeEnd")
         basicANimation.toValue = 1
         basicANimation.duration = 2
@@ -46,6 +83,8 @@ class ViewController: UIViewController {
         shapeLayer.add(basicANimation, forKey: "basic")
     }
 
-
+    @objc func objectTapped() {
+//        animateCircle()
+        beginDownloading()
+    }
 }
-
