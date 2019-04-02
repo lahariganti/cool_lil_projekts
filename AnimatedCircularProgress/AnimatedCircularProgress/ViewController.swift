5339f84894271408ecc3d5d1d73b4eda913a28f2
Lahari Ganti <gplahari@gmail.com>
2019-04-01 18:21:36 -0700

    .

--- AnimatedCircularProgress/AnimatedCircularProgress/ViewController.swift
+++ AnimatedCircularProgress/AnimatedCircularProgress/ViewController.swift
@@ -10,8 +10,8 @@ import UIKit
 
 class ViewController: UIViewController, URLSessionDownloadDelegate {
     let urlString = "https://firebasestorage.googleapis.com/v0/b/firestorechat-e64ac.appspot.com/o/intermediate_training_rec.mp4?alt=media&token=e20261d0-7219-49d2-b32d-367e1606500c"
-    let shapeLayer = CAShapeLayer()
-    let trackLayer = CAShapeLayer()
+    var shapeLayer = CAShapeLayer()
+    var trackLayer = CAShapeLayer()
     let percentageLabel: UILabel = {
         let label = UILabel()
         label.text = "Start"
@@ -23,39 +23,43 @@ class ViewController: UIViewController, URLSessionDownloadDelegate {
 
     var pulsatingLayer = CAShapeLayer()
 
+    let circularPath = UIBezierPath(arcCenter: .zero, radius: 80, startAngle: 0, endAngle: 2 * CGFloat.pi, clockwise: true)
+
+    private func createLayer(strokeColor: UIColor, fillColor: UIColor) -> CAShapeLayer {
+        let layer = CAShapeLayer()
+        layer.path = circularPath.cgPath
+        layer.strokeColor = strokeColor.cgColor
+        layer.lineWidth = 20
+        layer.lineCap = .round
+        layer.fillColor = fillColor.cgColor
+        layer.position = view.center
+        return layer
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor.backgroundColor
 
-        let circularPath = UIBezierPath(arcCenter: .zero, radius: 80, startAngle: 0, endAngle: 2 * CGFloat.pi, clockwise: true)
+        setupLayers()
+        setupPercentageLabel()
+    }
 
-        pulsatingLayer.path = circularPath.cgPath
-        pulsatingLayer.strokeColor = UIColor.clear.cgColor
-        pulsatingLayer.lineWidth = 20
-        pulsatingLayer.lineCap = .round
-        pulsatingLayer.fillColor = UIColor.pulsatingFillColor.cgColor
-        pulsatingLayer.position = view.center
-        view.layer.addSublayer(pulsatingLayer)
+    private func setupLayers() {
+        pulsatingLayer = createLayer(strokeColor: UIColor.clear, fillColor: UIColor.pulsatingFillColor)
         animatingPulsatingLayer()
 
-        trackLayer.path = circularPath.cgPath
-        trackLayer.strokeColor = UIColor.trackStrokeColor.cgColor
-        trackLayer.lineWidth = 20
-        trackLayer.lineCap = .round
-        trackLayer.fillColor = UIColor.backgroundColor.cgColor
-        trackLayer.position = view.center
-        view.layer.addSublayer(trackLayer)
+        trackLayer = createLayer(strokeColor: UIColor.trackStrokeColor, fillColor: UIColor.backgroundColor)
 
-        shapeLayer.path = circularPath.cgPath
-        shapeLayer.position = view.center
-        shapeLayer.strokeColor = UIColor.outlineStrokeColor.cgColor
-        shapeLayer.lineWidth = 20
+        shapeLayer = createLayer(strokeColor: UIColor.outlineStrokeColor, fillColor: UIColor.clear)
         shapeLayer.strokeEnd = 0
-        shapeLayer.lineCap = .round
-        shapeLayer.fillColor = UIColor.clear.cgColor
         shapeLayer.transform = CATransform3DMakeRotation(-CGFloat.pi / 2, 0, 0, 1)
-        view.layer.addSublayer(shapeLayer)
 
+        for layer in [pulsatingLayer, trackLayer, shapeLayer] {
+            view.layer.addSublayer(layer)
+        }
+    }
+
+    private func setupPercentageLabel() {
         view.addSubview(percentageLabel)
         percentageLabel.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
         percentageLabel.center = view.center
@@ -84,7 +88,7 @@ class ViewController: UIViewController, URLSessionDownloadDelegate {
     }
 
     func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didFinishDownloadingTo location: URL) {
-        print("done")
+        print("done downloading")
     }
 
     func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didWriteData bytesWritten: Int64, totalBytesWritten: Int64, totalBytesExpectedToWrite: Int64) {
